d4638fe0d677bd76f01415a7eea8ec1a9dd09116
Mike Bayer <mike_mp@zzzcomputing.com>
2015-01-21 18:03:03 -0500

    copyright

--- docs/build/conf.py
+++ docs/build/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dogpile.cache'
-copyright = u'2011-2014 Mike Bayer'
+copyright = u'2011-2015 Mike Bayer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
