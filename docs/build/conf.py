6c5d5b0eca8b284257c27fb8b4ead5da3aadf7d5
Mike Bayer <mike_mp@zzzcomputing.com>
2020-04-29 10:13:12 -0400

    happy new year
    Change-Id: I5af87f8526a01074f0ef6acd29a984cd3d272aaa

--- docs/build/conf.py
+++ docs/build/conf.py
@@ -65,7 +65,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"dogpile.cache"
-copyright = u"2011-2019 Mike Bayer"
+copyright = u"2011-2020 Mike Bayer"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
