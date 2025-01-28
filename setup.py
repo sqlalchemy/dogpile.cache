7b6263824073cda0992ad048f981c26a0c1930f1
Mike Bayer <mike_mp@zzzcomputing.com>
2025-01-28 08:42:52 -0500

    remove setuptools test command
    not sure how this was here for this long
    
    Change-Id: I49e8bddb09f7e6fd63d1580d1f4e12c9d59f4d9e

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-import sys
-
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 
-class UseTox(TestCommand):
-    RED = 31
-    RESET_SEQ = "\033[0m"
-    BOLD_SEQ = "\033[1m"
-    COLOR_SEQ = "\033[1;%dm"
-
-    def run_tests(self):
-        sys.stderr.write(
-            "%s%spython setup.py test is deprecated by pypa.  Please invoke "
-            "'tox' with no arguments for a basic test run.\n%s"
-            % (self.COLOR_SEQ % self.RED, self.BOLD_SEQ, self.RESET_SEQ)
-        )
-        sys.exit(1)
-
-
-setup(
-    cmdclass={"test": UseTox},
-)
+setup()
