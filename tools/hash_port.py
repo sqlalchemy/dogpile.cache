991e9808dde553cf56f9552e68eae7f4de672995
Mike Bayer <mike_mp@zzzcomputing.com>
2025-10-11 11:50:59 -0400

    move hash_port.py to tools/
    also remove log_tests.ini that seems to be unused,
    there's no CHANGES top level either
    
    Change-Id: I4ce48e253fe93c331f6ce6d94b4e7532dff495a5

---
+++ tools/hash_port.py
@@ -0,0 +1,27 @@
+"""
+Helper script which provides an integer number from a given range based on a
+hash of current directory name.
+
+This is used in continuous integration as a helper to provide ports to assign
+to services like Redis, Memcached when they are run on a per-test basis.
+
+E.g. in a Jenkins job, one could put as the run command::
+
+    export TOX_DOGPILE_PORT=`python tools/hash_port.py 10000 34000`
+    nox -v -t ${pyv}-${backend}
+
+So you'd get one TOX_DOGPILE_PORT for the script in
+/var/lib/jenkins-workspace/py27-redis, another TOX_DOGPILE_PORT for the script
+in /var/lib/jenkins-workspace/py34-memcached. nox (as well as the previous tox
+setup) calls the pifpaf tool to run redis/ memcached local to that build and
+has it listen on this port.
+"""
+
+import os
+import sys
+
+start, end = int(sys.argv[1]), int(sys.argv[2])
+
+dir_ = os.getcwd()
+
+print((hash(dir_) % (end - start)) + start)
